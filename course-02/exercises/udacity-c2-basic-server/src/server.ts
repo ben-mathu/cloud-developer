c6e5576b5f1021ab140435525072954700328004
Benard Mathu <mathubenard@gmail.com>
2022-08-23 20:31:34 +0300

    complete exercises

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -72,13 +72,67 @@ import { Car, cars as cars_list } from './cars';
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+  app.get("/cars", (req, res) => {
+    const { make } = req.query;
+    let car_item_list = cars_list;
+    if (make) {
+      car_item_list = cars_list.filter((car: Car) => car.make === make);
+    }
+    res.status(200).send(car_item_list);
+  });
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get("/cars/:id", (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+      res.status(400).send({"message": "Parameter id is required"});
+      return;
+    }
+
+    const car = cars_list.filter((car: Car) => car.id.toString() == id);
+
+    if (car && car.length > 0) {
+      res.status(200).send(car[0]);
+    } else {
+      res.status(404).send({"message": `Car with id ${id} was not found` })
+    }
+  });
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+  app.post("/cars", (req, res) => {
+    const { make, type, model, cost} = req.body;
+    let { id } = req.body;
+
+    if (make && type && model && cost) {
+      let cars: Car[] = cars_list.sort();
+      if (!id) {
+        id = cars[cars_list.length-1].id + 1
+      }
+
+      cars = cars_list.filter((car: Car) => car.id === id);
+      if (cars && cars.length > 0) {
+        res.status(400).send({"message": `car with id ${id} already exists` })
+        return;
+      }
+      
+      const car: Car = {
+        id: id,
+        make: make,
+        type: type,
+        model: model,
+        cost: cost
+      };
+
+      cars_list.push(car);
+
+      res.status(200).send(cars_list);
+    } else {
+      res.status(404).send({"message": "all parameters required" })
+    }
+  })
 
   // Start the Server
   app.listen( port, () => {
