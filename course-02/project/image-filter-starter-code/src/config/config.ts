3faa08172aa9bc10125cf1df64fb7fd6ac839aa5
Benard Mathu <mathubenard@gmail.com>
2022-08-28 16:15:17 +0300

    update with authentication and authorization

--- course-02/project/image-filter-starter-code/src/config/config.ts
+++ course-02/project/image-filter-starter-code/src/config/config.ts
@@ -11,7 +11,8 @@ export const config ={
     "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
-  "jwt": {
-    "secret": process.env.SECRET
+  "security": {
+    "secret": process.env.SECRET,
+    "saltRound": 10
   }
 }
--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,12 +1,16 @@
 import fs from 'fs';
 import axios from "axios";
 import Jimp = require("jimp");
-import { getPutFileSignedUrl, s3, s3Client } from "../aws";
-import FormData from 'form-data';
-import { Agent } from 'https';
-import { S3 } from 'aws-sdk';
+import { s3Client } from "../aws";
 import { config } from '../config/config';
-import { CreateBucketCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import * as bcrypt from 'bcrypt';
+import { User } from '../controllers/users/models/User';
+import * as jwt from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
+
+const cAws = config.aws;
+const cSecurity = config.security;
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -16,7 +20,6 @@ import { CreateBucketCommand, PutObjectCommand, S3Client } from '@aws-sdk/client
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string> {
-  const c = config.aws
   
   return new Promise(async (resolve, reject) => {
     try {
@@ -36,7 +39,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
         .greyscale() // set greyscale
         .write(filePath, async (img) => {
           const params = {
-            Bucket: c.aws_media_bucket,
+            Bucket: cAws.aws_media_bucket,
             Key: fileName,
             Body: fs.createReadStream(filePath)
           };
@@ -45,34 +48,6 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
 
           console.log(data);
           resolve(filePath);
-
-          // photoBucket.upload({
-          //   ACL: "public-read",
-          //   Body: fs.createReadStream(outpath),
-          //   Key: fileName,
-          //   ContentType: "application/octet-stream"
-          // }, (error, response) => {
-          //   console.log(error);
-          // });
-          // axios({
-          //   method: 'put',
-          //   url: url,
-          //   data: form,
-          //   headers: headers,
-          //   httpsAgent: new Agent({
-          //     rejectUnauthorized: false
-          //   }),
-          //   maxContentLength: Infinity,
-          //   maxBodyLength: Infinity,
-          //   onUploadProgress: (progressEvent: any) => {
-          //     const complete = (progressEvent.loaded / progressEvent.total * 100 | 0) + '%';
-          //     console.log('upload percent: ' + complete);
-          //   }
-          // })
-          //   .then((result) => resolve(fileName)
-          //   ).catch((error) => {
-          //     reject(error);
-          //   });
         });
     } catch (error) {
       reject(error);
@@ -89,4 +64,39 @@ export async function deleteLocalFiles(files: Array<string>) {
   for (let file of files) {
     fs.unlinkSync(file);
   }
+}
+
+export async function getHashedPassword(plainTextPassword: string): Promise<string> {
+  const salt = await bcrypt.genSalt(cSecurity.saltRound as unknown as number);
+  const hash = await bcrypt.hash(plainTextPassword, salt);
+
+  return hash;
+}
+
+export async function comparePassword(plainTextPassword: string, hashedPassword: string): Promise<boolean> {
+  return await bcrypt.compare(plainTextPassword, hashedPassword);
+}
+
+export async function generateToken(user: User) {
+  return jwt.sign(user.toJSON(), cSecurity.secret);
+}
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (!req.headers && !req.headers.authorization) {
+    return res.status(400).send({message: 'No Authrization Headers'});
+  }
+
+  const tokenBearerStrArr = req.headers.authorization.split(' ');
+  if (tokenBearerStrArr.length != 2 || tokenBearerStrArr[0] != 'Bearer') {
+    return res.status(400).send({message: 'Wrong Authorization configuration'});
+  }
+
+  const token = tokenBearerStrArr[1];
+
+  return jwt.verify(token, cSecurity.secret, (error, decoded) => {
+    if (error) {
+      return res.status(400).send({message: 'Failed to authenticate'});
+    }
+    return next();
+  })
 }
